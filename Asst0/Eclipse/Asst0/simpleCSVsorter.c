228b6b10deefc3a2910e0a5c8ef5d21b6658e9ad
Forrest Smith <fsmith2017@gmail.com>
2018-09-29 09:12:19 -0400

    Basic Framework & Newline Test

--- Asst0/Eclipse/Asst0/simpleCSVsorter.c
+++ Asst0/Eclipse/Asst0/simpleCSVsorter.c
@@ -8,7 +8,20 @@
 
 
 int main(int argc, char ** argv) {
-	printf("%d", 69);
+
+	printf("%s", argv[1]);
+
+	int r = 1;
+
+	FILE* fp = NULL;
+	fp = fopen(argv[1], "r");
+
+	while(r != EOF)
+	{
+	char* headers = (char*) malloc(1000*sizeof(char));
+	r = fscanf(fp, "%s\n", headers);
+	printf("%s___\n", headers);
+	}
 
 	return 0;
 }
--- Asst0/Eclipse/Asst0/simpleCSVsorter.h
+++ Asst0/Eclipse/Asst0/simpleCSVsorter.h
@@ -14,6 +14,18 @@
 #ifndef SIMPLECSVSORTER_H_
 #define SIMPLECSVSORTER_H_
 
+typedef enum _format{NUM, STR} format;
 
+typedef struct Column {		// Only for first row data
+	char* entry;			// Hold the titles of each column
+	format type;		// Declare the types of each column, corresponds with row entry
+} Column;
+
+typedef struct Row {
+	char** entry;			// Hold each entry of the row. The type of each corresponds to format enum
+} Row;
+
+Column* Columns;
+Row* Rows;
 
 #endif /* SIMPLECSVSORTER_H_ */
