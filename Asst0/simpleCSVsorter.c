3a75bc584dc787c022c2b4e50b3b07b541093873
jchan601 <justchanabc@yahoo.com>
2018-10-01 00:22:31 -0400

    print p for pointers??

--- Asst0/simpleCSVsorter.c
+++ Asst0/simpleCSVsorter.c
@@ -272,7 +272,7 @@ int main(int argc, char ** argv) {
 	int index = -1; // index of column to sort on
 	char* colname = argv[2];
 	for (i = 0; i < c; i++) {
-		printf("colname: %d %s, header.titles[%d]: %d, %s", (int)colname, colname, i, (int)header.titles[i], header.titles[i]);
+		printf("colname: %p %s, header.titles[%d]: %p, %s", colname, colname, i, header.titles[i], header.titles[i]);
 		if (strcmp(colname, header.titles[i]) == 0) {
 			index = i;
 			break;
