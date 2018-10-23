da62b9832e510cdd03257eaa78c69fbf15869d77
Forrest Smith <fsmith2017@gmail.com>
2018-10-22 23:01:29 -0400

    Asst1 Added

---
+++ Asst1/Eclipse/Asst0/mergesort.c
@@ -0,0 +1,209 @@
+/*
+ * mergesort.c
+ *
+ *  Created on: Sep 28, 2018
+ *      Author: Justin Chan, Forrest Smith
+ */
+
+#include "simpleCSVsorter.h"
+
+/**
+ * Compares two strings
+ * @param str1 First string
+ * @param str2 Second string
+ * @return number > 0 if str1 comes after str2, 0 if they are equal, number < 0 if str1 comes before str2
+ */
+int stringComparator(void* str1, void* str2) {
+	return strcmp((char*)str1, (char*)str2);
+}
+
+/**
+ * Compares two integers
+ * @param n1 First integer
+ * @param n2 Second integer
+ * @return number > 0 if n1 > n2, 0 if n1 = n2, number < 0 if n1 < n2
+ */
+int intComparator(void* n1, void* n2) {
+	return *((int*)n1) - *((int*)n2);
+}
+
+/**
+ * Compares two doubles
+ * @param n1 First double
+ * @param n2 Second double
+ * @return number > 0 if n1 > n2, 0 if n1 = n2, number < 0 if n1 < n2
+ */
+int doubleComparator(void* n1, void* n2) {
+	double d = *((double*)n1) - *((double*)n2);
+	if (d < 0.0) {
+		return -1;
+	} else if (d > 0.0) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+/**
+ * Trims white space and removes quotation marks from a string
+ * @param str String to parse
+ * @return Formatted string
+ */
+char* trim(char* str) {
+	int length = strlen(str);
+	char * ret = (char*)malloc(sizeof(char) * (length + 1));
+	if (ret == NULL) {
+		printf("ERROR: malloc returned null at formatString in mergesort.c");
+		return str;
+	}
+	int j = 0;
+	int i = 0;
+	char prev = '\0';
+	char firstNonSpaceChar = '\0';
+	short started = 0; // for determining whether string started or not... quotes and spaces don't count as start
+	for (i = 0; i < length; i++) {
+		if (started == 0 && (str[i] == '"' || str[i] == ' ')) {
+			if (str[i] == '"' && firstNonSpaceChar == '\0') {
+				firstNonSpaceChar = '"';
+			}
+			continue;
+		}
+		if (str[i] == '"' && i == length - 1 && firstNonSpaceChar == '"') {
+			break;
+		}
+		if (str[i] == ' ') {
+			prev = ' ';
+			continue;
+		}
+		if (isspace(str[i])) {
+			continue;
+		}
+
+		if (prev == ' ') {
+			ret[j] = ' ';
+			j++;
+		}
+
+		if (started == 0) {
+			started = 1;
+		}
+
+		ret[j] = str[i];
+		j++;
+		prev = str[i];
+	}
+	ret[j] = '\0';
+	return ret;
+}
+
+/**
+ * Merges two arrays of rows. Used for merge sort
+ * @param arrows1 First array of Row structs to be merged
+ * @param length1 Length of first array
+ * @param arrow2 Second array of Row structs to be merged
+ * @param length2 Length of second array
+ * @param index Index of the value being sorted in the row
+ * @param f Format enum indicating whether value to be sorted is an integer or a string
+ * @return Sorted array of Rows
+ */
+Row* merge(Row* arrows1, int length1, Row* arrows2, int length2, int index, format f) {
+
+	Row* result = (Row*)malloc(sizeof(Row) * (length1 + length2)); // Auxiliary array containing result
+	if (result == NULL) {
+		printf("ERROR: malloc failed\n");
+		return NULL;
+	}
+	int lIndex = 0; // Current index in left array
+	int rIndex = 0; // Current index in right array
+	int aIndex = 0; // Current index in auxiliary array
+
+	if (f == INTEGER) {
+		while (lIndex < length1 && rIndex < length2) {
+			char* e1 = arrows1[lIndex].entries[index];
+			char* e2 = arrows2[rIndex].entries[index];
+			if (strcmp(e1, "") == 0) {
+				result[aIndex++] = arrows1[lIndex++];
+			} else if (strcmp(e2, "") == 0) {
+				result[aIndex++] = arrows2[rIndex++];
+			} else {
+				double n1 = atof(e1);
+				double n2 = atof(e2);
+				if (doubleComparator(&n1, &n2) < 0) {
+					result[aIndex++] = arrows1[lIndex++];
+				} else {
+					result[aIndex++] = arrows2[rIndex++];
+				}
+			}
+		}
+	} else if (f == DOUBLE) {
+		while (lIndex < length1 && rIndex < length2) {
+			char* e1 = arrows1[lIndex].entries[index];
+			char* e2 = arrows2[rIndex].entries[index];
+			if (strcmp(e1, "") == 0) {
+				result[aIndex++] = arrows1[lIndex++];
+			} else if (strcmp(e2, "") == 0) {
+				result[aIndex++] = arrows2[rIndex++];
+			} else {
+				double d1 = atof(e1);
+				double d2 = atof(e2);
+				if (doubleComparator(&d1, &d2) < 0) {
+					result[aIndex++] = arrows1[lIndex++];
+				} else {
+					result[aIndex++] = arrows2[rIndex++];
+				}
+			}
+		}
+	} else if (f == STRING) {
+		while (lIndex < length1 && rIndex < length2) {
+			char* e1 = arrows1[lIndex].entries[index];
+			char* e2 = arrows2[rIndex].entries[index];
+			if (strcmp(e1, "") == 0) {
+				result[aIndex++] = arrows1[lIndex++];
+			} else if (strcmp(e2, "") == 0) {
+				result[aIndex++] = arrows2[rIndex++];
+			} else {
+				char* s1 = trim(e1);
+				char* s2 = trim(e2);
+				if (stringComparator(s1, s2) < 0) {
+					result[aIndex++] = arrows1[lIndex++];
+				} else {
+					result[aIndex++] = arrows2[rIndex++];
+				}
+				free(s1);
+				free(s2);
+			}
+		}
+	} else {
+		return NULL;
+	}
+
+	while (lIndex < length1) {
+		result[aIndex++] = arrows1[lIndex++];
+	}
+
+	while (rIndex < length2) {
+		result[aIndex++] = arrows2[rIndex++];
+	}
+
+	return result;
+	// Remember to free after doing mergesort
+}
+
+/**
+ * Sorts an array of Rows using merge sort
+ * @param arrows Array of Row structs to be sorted
+ * @param length Length of array
+ * @param index Index of the value being sorted in the row
+ * @param f Format enum indicating whether value to be sorted is an integer or a string
+ * @return Sorted array of Rows
+ */
+Row* mergeSort(Row* arrows, int length, int index, format f) {
+	if (length <= 1) {
+		return arrows;
+	}
+	int l = length / 2; // Length of left subarray
+	int r = l + length % 2; // Length of right subarray
+	Row* m1 = mergeSort(arrows, l, index, f);
+	Row* m2 = mergeSort(&arrows[l], r, index, f);
+	return merge(m1, l, m2, r, index, f);
+}
---
+++ Asst1/scannerCSVsorter.c
@@ -0,0 +1,344 @@
+/*
+ * simpleCSVsorter.c
+ *
+ *  Created on: Sep 20, 2018
+ *      Author: Justin Chan, Forrest Smith
+ */
+
+#include "scannerCSVsorter.h"
+
+
+int SetHeader(Header* h) {
+
+	size_t length = 0;
+	char* line = NULL;
+	getline(&line, &length, stdin);
+	if (feof(stdin)) {
+		return -1;
+	}
+
+	int quotes = 0;
+	int arrsize = 1;
+	int position = 0;
+
+	size_t i = 0;
+	size_t linepos = 0;
+	size_t linelength = strlen(line);
+
+	while (linepos < linelength) {
+
+		if (position >= arrsize) {
+			arrsize *= 2;
+			h->titles = (char**)realloc(h->titles, sizeof(char*) * (arrsize + 1));
+			if (h->titles == NULL) {
+				fprintf(stderr, "ERROR: realloc failed\n");
+				return -1;
+			}
+		}
+
+		i = 0;
+		size_t entrylength = 30;
+		char* entry = (char*) malloc((entrylength + 1) * sizeof(char));
+		if (entry == NULL) {
+			fprintf(stderr, "ERROR: malloc failed\n");
+			return -1;
+		}
+
+		int a = 0;
+		for (a = 0; a < entrylength; a++) {
+			entry[a] = '\0';
+		}
+
+		while (linepos < linelength) {
+			char c = line[linepos];
+
+			if (c == '"' && quotes == 0)
+				quotes++;
+			else if (c == '"' && quotes == 1)
+				quotes--;
+
+			if ((c == ',' && quotes == 0) || c == '\n') {
+				linepos++;
+				break;
+			}
+
+			if (c == '\r') {
+				linepos++;
+				continue;
+			}
+
+			entry[i] = line[linepos];
+
+			i++;
+			linepos++;
+			if (i >= entrylength) {
+				entrylength *= 2;
+				entry = (char*)realloc(entry, sizeof(char) * (entrylength + 1));
+				if (entry == NULL) {
+					fprintf(stderr, "ERROR: realloc failed\n");
+					return -1;
+				}
+				for (a = entrylength / 2 + 2; a < entrylength; a++) {
+					entry[a] = '\0';
+				}
+			}
+		}
+		h->titles[position] = entry;
+		position++;
+	}
+
+	return position;
+}
+
+/**
+ * Reads current row and returns array of entries
+ */
+
+char** GetLine() {
+
+	// Use getLine to get current line and store in "line" variable
+	size_t length = 0;
+	char* line = NULL;
+	getline(&line, &length, stdin);
+	if (feof(stdin)) {
+		return NULL;
+	}
+
+	// Create 2D array to hold each entry
+	int quotes = 0;
+	int arrsize = 1;
+	int position = 0;
+	char** entries = (char**) malloc(arrsize * sizeof(char*));
+	if (entries == NULL) {
+		fprintf(stderr, "ERROR: malloc failed\n");
+		return NULL;
+	}
+
+	// Parse for commas and quotes
+	size_t i = 0;
+	size_t linepos = 0;
+	size_t linelength = strlen(line);
+
+	// While still entering entries
+	while (linepos < linelength) {
+
+		// If we have more entries than array size, double array length
+		if (position >= arrsize) {
+			arrsize *= 2;
+			entries = (char**)realloc(entries, sizeof(char *) * arrsize);
+			if (entries == NULL) {
+				fprintf(stderr, "ERROR: realloc failed\n");
+				return NULL;
+			}
+		}
+
+		i = 0;
+		size_t entrylength = 30;
+		char* entry = (char*) malloc((entrylength + 1) * sizeof(char));
+		if (entry == NULL) {
+			fprintf(stderr, "ERROR: malloc failed\n");
+			return NULL;
+		}
+
+		int a = 0;
+		for (a = 0; a < entrylength; a++) {
+			entry[a] = '\0';
+		}
+
+		// While reading from the entries line
+		while (linepos < linelength) {
+			char c = line[linepos];
+
+			// Check for quotes
+			if (c == '"' && quotes == 0)
+				quotes++;
+			else if (c == '"' && quotes == 1)
+				quotes--;
+
+			// Check for commas sans quotes
+			if ((c == ',' && quotes == 0) || c == '\n') {
+				linepos++;
+				break;
+			}
+			entry[i] = line[linepos];
+
+			i++;
+			linepos++;
+			if (i >= entrylength) {
+				entrylength *= 2;
+				entry = (char*)realloc(entry, sizeof(char) * (entrylength + 1));
+				if (entry == NULL) {
+					fprintf(stderr, "ERROR: realloc failed\n");
+					return NULL;
+				}
+				for (a = entrylength / 2 + 2; a < entrylength; a++) {
+					entry[a] = '\0';
+				}
+			}
+		}
+		entries[position] = entry;
+		position++;
+	}
+
+	return entries;
+}
+
+format getType(char* str) {
+	int i = 0;
+	int n = strlen(str);
+	format ret = INTEGER;
+	int periods = 0;
+	for (i = 0; i < n; i++) {
+		if (i == 0 && str[i] == '-') {
+			continue;
+		} else if (!isdigit(str[i])) {
+			if (str[i] == '.' && periods == 0) {
+				ret = DOUBLE;
+				periods++;
+			} else {
+				ret = STRING;
+				break;
+			}
+		}
+	}
+	return ret;
+}
+
+int FillRows(Row** Rows, Header* header, int columns) {
+	int rows = 0;
+	int capacity = 1;
+	int w = 0;
+
+	while (1) {
+		char** entries = GetLine();
+		if (entries == NULL) {
+			return rows;
+		}
+
+		(*Rows)[rows].entries = entries;
+		for (w = 0; w < columns; w++) {
+			format t = getType(entries[w]);
+			if (t > header->types[w]) {
+				header->types[w] = t;
+			}
+		}
+		rows++;
+		if (rows >= capacity) {
+			capacity *= 2;
+			*Rows = (Row*)realloc(*Rows, capacity * sizeof(Row));
+			if (*Rows == NULL) {
+				fprintf(stderr, "ERROR: realloc failed\n");
+				return -1;
+			}
+			for (w = capacity / 2 + 2; w < capacity; w++) {
+				(*Rows)[w].entries = NULL;
+			}
+		}
+	}
+	return rows;
+}
+
+int GetIndex(char** source, char* target) {
+
+	if (!source || !target) {
+		fprintf(stderr, "Cannot find index of null source or null target");
+		return -1;
+	}
+
+	int i = 0;
+	while (source[i]) {
+		if (strcmp(source[i], target) == 0)
+			return i;
+		i++;
+	}
+
+	return -1;
+}
+
+int main(int argc, char ** argv) {
+
+	if (argc != 3) {
+		fprintf(stderr, "ERROR: Incorrect number of arguments. Correct usage is ./simpleCSVsorter <sortBy> <columnName>\nExample:./simpleCSVsorter -c food\n");
+		return 0;
+	}
+
+	if (strcmp(argv[1], "-c") != 0) {
+		fprintf(stderr, "ERROR: Invalid first argument found. Can only use -c to indicate sorting by column.\n");
+		return 0;
+	}
+
+	// Get column titles
+	Header header = { NULL, NULL };
+	header.titles = (char**) malloc(sizeof(char*) * 10);
+	if (header.titles == NULL) {
+		fprintf(stderr, "ERROR: malloc failed\n");
+		return 0;
+	}
+
+	int i = 0;
+	int c = SetHeader(&header); // Number of columns in table
+	if (c == -1) {
+		fprintf(stderr, "ERROR: No columns found.\n");
+		return 0;
+	}
+	header.types = (format*)malloc(sizeof(format) * c);
+	if (header.types == NULL) {
+		fprintf(stderr, "ERROR: malloc failed\n");
+		return 0;
+	}
+	for (i = 0; i < c; i++) {
+		header.types[i] = INTEGER;
+	}
+
+	Row* rows = (Row*)malloc(sizeof(Row));
+	if (rows == NULL) {
+		fprintf(stderr, "ERROR: malloc failed\n");
+		return 0;
+	}
+	rows[0].entries = NULL;
+
+	int rowcount = FillRows(&rows, &header, c);
+
+	int index = -1; // index of column to sort on
+	char* colname = argv[2];
+	for (i = 0; i < c; i++) {
+		if (strcmp(colname, header.titles[i]) == 0) {
+			index = i;
+			break;
+		}
+	}
+
+	if (index == -1) {
+		fprintf(stderr, "ERROR: '%s' is not a column found in this file.\n", colname);
+		return 0;
+	}
+
+	Row* out = mergeSort(rows, rowcount, index, header.types[index]);
+
+	free(rows);
+
+	int p = 0;
+	for (p = 0; p < c; p++) {
+		printf("%s", header.titles[p]); // print out the top row
+		if (p != c - 1) {
+			printf(",");
+		}
+	}
+	printf("\n");
+
+	for (i = 0; i < rowcount; i++) {
+		for (p = 0; p < c; p++){
+			printf("%s", out[i].entries[p]);
+			if (p != c - 1) {
+				printf(",");
+			}
+		}
+		printf("\n");
+	}
+
+	free(out);
+
+	printf("\n");
+
+	return 0;
+}
