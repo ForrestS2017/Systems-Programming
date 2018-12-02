4704301571ae8f42d3c1e57dd13acdc8456389de
Forrest Smith <fsmith2017@gmail.com>
2018-12-02 17:58:00 -0500

    Basic outline for Client

--- Asst3/bankingClient.c
+++ Asst3/bankingClient.c
@@ -1,6 +1,33 @@
 #include "bankingClient.h"
 
 int main(int argc, char** argv){
+	// Argument Format: ./bankingClient <machine> <port>
 
-	return 0;
+	// Verify command line arguments
+	if (argc != 3) {
+		fprintf(stderr, "ERROR: Invalid argument count");
+		//fprintf(stdout, "ERROR: Invalid argument count");
+		fflush(stderr);
+		exit(EXIT_FAILURE);
+	}
+
+	if(atoi(argv[2]) < 1) {
+		fprintf(stderr, "ERROR: Invalid port");
+	}
+
+	fflush(stderr);
+
+	// Begin server functionality
+	int port = atoi(argv[2]);
+	int server_fd, new_socket, valread;
+
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+    { 
+        fprintf(stderr,"socket failed"); 
+        fflush(stderr);
+		exit(EXIT_FAILURE);
+    } 
+
+	return 0; 
+	
 }
--- Asst3/bankingClient.h
+++ Asst3/bankingClient.h
@@ -7,3 +7,36 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/socket.h> 
+#include <sys/types.h>
+#include <netinet/in.h> 
+#include <netdb.h> 
+
+/** Global Variables **/
+
+
+/** Structs **/
+
+typedef struct _Account {
+    char name[255];
+    double balance;
+    int session;
+
+} Account;
+
+
+/** Functions **/
+
+int CreateAccount(char *);
+
+int ServeAccount(char *);
+
+int DepositAccount(double);
+
+int WithdrawACcount(double);
+
+double Query();
+
+int End();
+
+int Quit();
